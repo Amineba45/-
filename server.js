10f87755fb54766638a559a07d3dbd2ab738735b
Amineba45 <mouhamadoulamineba52@gmail.com>
2026-04-06 19:32:15 +0000

    Add server.js entry point file

---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./src/app');
+const logger = require('./src/config/logger');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    logger.info(`Samba Service Backend running on port ${PORT}`);
+});
