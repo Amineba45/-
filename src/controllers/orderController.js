7ef97deeb44b5bc5d376897b3fede24e14a9c37d
Amineba45 <mouhamadoulamineba52@gmail.com>
2026-04-06 19:31:20 +0000

    Add orderController.js implementing order functions

---
+++ src/controllers/orderController.js
@@ -0,0 +1,35 @@
+// orderController.js
+
+class OrderController {
+    // Create a new order
+    createOrder(req, res) {
+        // Implementation here
+    }
+
+    // Get all orders
+    getOrders(req, res) {
+        // Implementation here
+    }
+
+    // Get orders by user
+    getUserOrders(req, res) {
+        // Implementation here
+    }
+
+    // Get order by ID
+    getOrderById(req, res) {
+        // Implementation here
+    }
+
+    // Update order status
+    updateOrderStatus(req, res) {
+        // Implementation here
+    }
+
+    // Cancel an order
+    cancelOrder(req, res) {
+        // Implementation here
+    }
+}
+
+module.exports = new OrderController();
